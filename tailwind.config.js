714f60cbbede7c1ef6a0798933717270574d4ad1
csknitesh7 <91814259+csknitesh7@users.noreply.github.com>
2024-04-03 12:49:50 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
     screens: {
       sm: "640px",
       md: "768px",
-      lg: "960px",
+      lg: "850px",
       xl: "1200px",
     },
     extend: {
@@ -27,7 +27,7 @@ module.exports = {
         accent: "#F13024",
       },
       backgroundImage: {
-        explosion: 'url("/bg-explosion.png")',
+        explosion: 'url("/bg-ghantaghar.png")',
         circles: 'url("/bg-circles.png")',
         circleStar: 'url("/circle-star.svg")',
         site: 'url("/site-bg.svg")',
